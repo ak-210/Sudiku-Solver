7433873382be7ed80adbb75d7881e0335908c895
ak-210 <106259041+ak-210@users.noreply.github.com>
2023-05-10 01:01:54 +0530

    Update sudoku_solver.py

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,8 +1,11 @@
 class Board:
     def __init__(self):
+        # For taking input from the user
         # self.board = list([0]*9 for _ in range(9))
         # for i in range(9):
         #     self.board[i] = list(map(int, input().split()))
+        
+        # Default board used
         self.board = [
             [7,8,0,4,0,0,1,2,0],
             [6,0,0,0,7,5,0,0,9],
@@ -14,7 +17,8 @@ class Board:
             [1,2,0,0,0,7,4,0,0],
             [0,4,9,2,0,6,0,0,7]
         ]
-
+        
+    # Function to print the board in a way easier for the user to read
     def print_board(self):
         for i in range(9):
             if i % 3 == 0 and i != 0:
@@ -29,13 +33,14 @@ class Board:
                 else:
                     print(self.board[i][j] , end = ' ')
 
+    # Check if the number will be valid in the given square
     def valid(self, row, col, num):
         # Check row and column
         for i in range(9):
             if self.board[row][i] == num or self.board[i][col] == num:
                 return False
 
-        # Check in the box
+        # Check in the 3x3 box
         box_x = row//3 * 3
         box_y = col//3 * 3
 
@@ -45,28 +50,31 @@ class Board:
                     return False
 
         return True
-
+    
+    # Solving the board recursively
     def solve(self):
         for i in range(9):
             for j in range(9):
-                if self.board[i][j] == 0:
+                if self.board[i][j] == 0: # Check for empty space
                     for num in range(1, 10):
-                        if self.valid(i, j, num):
+                        if self.valid(i, j, num): # Trying all the numbers for a given square
+                            # If a number is valid
                             self.board[i][j] = num
                             if self.solve():
                                 return True
-                            self.board[i][j] = 0
+                            self.board[i][j] = 0 #Change the number to 0 and start from the next number as present one is invalid
 
-                    return False
+                    return False #Returns if none of the number is possible
 
-        return True
+        return True #Board is solved
         
 
 
 if __name__ == "__main__":
-    start = Board()
-    start.print_board()
-    start.solve()
+    grid = Board()
+    print('Input Puzzle:')
+    grid.print_board()
+    grid.solve()
     print('\n Solution:')
-    start.print_board()
+    grid.print_board()
 
